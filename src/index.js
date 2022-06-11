6c78a5a3f54003a53a0d62db0fa4419727a5d3dd
fariasagustin3 <fariasagustin3@gmail.com>
2022-06-11 11:52:07 -0300

    cors module added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const allTasks = require('./routes/tasks.routes');
+const cors = require('cors')
 
 const morgan = require('morgan');
 
+app.use(cors()); 
 app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/tasks', allTasks)
+
+// middleware to handle server errors
+app.use((err, req, res, next) => {
+    return res.json({ msg: err.message})
+})
 
 
 app.listen(4000)
